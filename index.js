6f95799b13ac1785c612f7f435d4a5f1ecfa72b8
turbobeast <georgemichaelfaust@gmail.com>
2015-10-19 15:32:38 -0400

    first commit -- works with node, not setup for angular/in-browser yet

---
+++ index.js
@@ -0,0 +1,16 @@
+// Copyright 2015 Bubl Technology Inc.
+//
+// Licensed under the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>.
+// This file may not be copied, modified, or distributed
+// except according to those terms.
+
+'use strict';
+
+var OscClient = require('./lib/OscClient');
+var BublOscClient = require('./lib/BublOscClient')
+
+module.exports = {
+  OscClient: OscClient,
+  BublOscClient: BublOscClient
+};
